3d893cf51c8c92e697f5ba0a720f72dad0af80ba
zakariae <zakariae@DESKTOP-6BRE4CL>
2022-04-14 13:32:16 +0000

    change

--- src/sportorg/SportOrg.java
+++ src/sportorg/SportOrg.java
@@ -16,7 +16,7 @@ public class SportOrg {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("dfrfrejin");
+        System.out.println("dfr");
         
         
     }
