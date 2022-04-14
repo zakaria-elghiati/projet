253f0a81cc4fc8ce74a6b0fe32d9357b24cde479
zakariae <zakariae@DESKTOP-6BRE4CL>
2022-04-14 13:04:25 +0000

    rani zdt

--- src/sportorg/SportOrg.java
+++ src/sportorg/SportOrg.java
@@ -16,7 +16,7 @@ public class SportOrg {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Jlabel;
+        System.out.println("d");
     }
     
 }
